5d0270a6211833e2e9fda731ee10dbfa683c9dd9
grady <grady_lee@163.com>
2022-07-25 10:33:27 +0800

    master commit2

--- src/main/java/com/turing/MyTest.java
+++ src/main/java/com/turing/MyTest.java
@@ -5,6 +5,7 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello dev");
+        System.out.println("hello master");
     }
 
 }
